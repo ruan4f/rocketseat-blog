986b8991ebb903018869a3837a1918dd981a72a9
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-06-15 20:54:36 -0300

    Adjust

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
 import styles from './header.module.scss';
+import Link from 'next/link';
 
 export default function Header() {
 
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
+        <a href="/">
           <img src="/images/logo.svg" alt="logo" />
+        </a>
       </div>
     </header>
   );

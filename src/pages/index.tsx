8375f6e308851e17286b0a77221a8eda536c4b39
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-06-17 11:06:22 -0300

    Changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { useState } from 'react';
 
 interface Post {
   uid?: string;
@@ -28,8 +29,29 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home({ postsPagination: { next_page, results } }: HomeProps) {
-  // TODO
+export default function Home({ postsPagination }: HomeProps) {
+  const [posts, setPosts] = useState(postsPagination.results);
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
+
+  async function hancleMorePosts(): Promise<void> {  
+    await fetch(postsPagination.next_page)
+      .then(res => res.json())
+      .then(jsonData => {
+        const newPosts = jsonData.results.map(post => {
+          return {
+            uid: post.uid,
+            first_publication_date: format(new Date(post.first_publication_date), "d MMM yyyy", { locale: ptBR, }),
+            data: {
+              title: post.data.title,
+              subtitle: post.data.subtitle,
+              author: post.data.author
+            },
+          };
+        });
+        setPosts(oldPosts => [...oldPosts, ...newPosts]);
+        setNextPage(jsonData.next_page);
+      });
+  }
 
   return (
     <>
@@ -37,22 +59,37 @@ export default function Home({ postsPagination: { next_page, results } }: HomePr
         <title>Blog | Home</title>
       </Head>
 
-      <main className={styles.contentContainer}>
-        <div className={styles.posts}>
-          {results.map((post: Post) => (
+      <main className={styles.postContainer}>
+        <div className={styles.postList}>
+          {posts.map(post => (
             <Link key={post.uid} href={`/post/${post.uid}`}>
               <a>
                 <strong>{post.data.title}</strong>
                 <p>{post.data.subtitle}</p>
-                <span>
-                  <FiCalendar /><time>{post.first_publication_date}</time>
-                  <FiUser />{post.data.author}
-                </span>
+
+                <div className={styles.postInfo}>
+                  <time>
+                    <FiCalendar />
+                    {post.first_publication_date}
+                  </time>
+                  <span>
+                    <FiUser />
+                    {post.data.author}
+                  </span>
+                </div>
               </a>
             </Link>
           ))}
-          {next_page ? <button>Carregar mais posts</button> : ''}
         </div>
+        {nextPage && (
+          <button
+            className={styles.loadMorePostsButton}
+            type="button"
+            onClick={hancleMorePosts}
+          >
+            Carregar mais posts
+          </button>
+        )}
       </main>
     </>
   );
@@ -60,7 +97,9 @@ export default function Home({ postsPagination: { next_page, results } }: HomePr
 
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient({});
-  const postsResponse = await prismic.getByType('posts');
+  const postsResponse = await prismic.getByType('posts', { pageSize: 1 });
+
+  console.log(JSON.stringify(postsResponse, null, 2));
 
   const posts = postsResponse.results.map(post => {
     return {
